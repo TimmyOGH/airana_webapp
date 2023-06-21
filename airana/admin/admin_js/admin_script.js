2829ed9acccb9bd992b90992f879537adfa36439
EthernalEnvy1111 <136286732+EthernalEnvy1111@users.noreply.github.com>
2023-06-21 15:04:27 +0630

    Update admin_script.js

--- airana/admin/admin_js/admin_script.js
+++ airana/admin/admin_js/admin_script.js
@@ -8,3 +8,16 @@ function toggleAddAdminPopUp() {
 function addAdmin() {
     toggleAddAdminPopUp();
 }
+
+//changing background after choosing file
+function changeBackground(event, targetId) {
+    var div = document.getElementById(targetId);
+    var file = event.target.files[0];
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+    div.style.backgroundImage = "url('" + e.target.result + "')";
+    }
+
+    reader.readAsDataURL(file);
+}
