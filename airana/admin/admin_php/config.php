013b4e28398a00e435515021e9e370e442819989
TimmyOGH <75523747+TimmyOGH@users.noreply.github.com>
2023-06-29 19:13:17 +0630

    Update config.php

--- airana/admin/admin_php/config.php
+++ airana/admin/admin_php/config.php
@@ -1,1 +1,17 @@
+<?php 
+// func to connect to DB
+function connect() {
+    $dsn = "mysql:dbname=airanaDB";
+    $username = "root";
+    $password = "4HISGlory";
 
+    try {
+        $conn = new PDO($dsn, $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+    
+    return $conn;
+}
+?>
