67480535e33ce76bfb313c4bc0d8ed07ed74532e
TimmyOGH <75523747+TimmyOGH@users.noreply.github.com>
2023-06-29 19:04:54 +0630

    Update admin_script.js

--- airana/admin/admin_js/admin_script.js
+++ airana/admin/admin_js/admin_script.js
@@ -1,12 +1,32 @@
-// show/hide pop-up
+// show/hide add admin pop-up
 function toggleAddAdminPopUp() {
-    var overlay = document.querySelector('.overlay');
-    overlay.classList.toggle('active');
+    var add_admin_overlay = document.querySelector('.add_admin_overlay');
+    add_admin_overlay.classList.toggle('active');
 }
 
-// handle form submission
-function addAdmin() {
-    toggleAddAdminPopUp();
+// show/hide edit admin pop-up
+function toggleEditAdminPopUp() {
+    var edit_admin_overlay = document.querySelector('.edit_admin_overlay');
+    edit_admin_overlay.classList.toggle('active');
+}
+
+// show/hide delete admin pop-up
+function toggleDeleteAdminPopUp() {
+    var delete_admin_overlay = document.querySelector('.delete_admin_overlay');
+    delete_admin_overlay.classList.toggle('active');
+}
+
+// hide admin login pop up & remove blur effect
+function hideAdminLoginPopUp() {
+    var adminLoginContainer = document.getElementById('admin-login-container');
+    adminLoginContainer.style.display = 'none';
+}
+
+// Check if user is logged in using php session
+var loggedIn = "<?php echo isset($_SESSION['adminLoggedIn']) && $_SESSION['adminLoggedIn'] ? 'true' : 'false'; ?>";
+
+if (loggedIn === 'true') {
+    hideAdminLoginPopUp();
 }
 
 //changing background after choosing file
@@ -15,9 +35,10 @@ function changeBackground(event, targetId) {
     var file = event.target.files[0];
     var reader = new FileReader();
 
-    reader.onload = function(e) {
-    div.style.backgroundImage = "url('" + e.target.result + "')";
+    reader.onload = function (e) {
+        div.style.backgroundImage = "url('" + e.target.result + "')";
     }
 
     reader.readAsDataURL(file);
 }
+
