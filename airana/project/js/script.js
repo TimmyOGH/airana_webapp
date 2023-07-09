c67dfcf8bd980a1fa68038f5db26bd9fa7869340
TimmyOGH <75523747+TimmyOGH@users.noreply.github.com>
2023-07-09 12:42:28 +0630

    Update script.js

--- airana/project/js/script.js
+++ airana/project/js/script.js
@@ -1,3 +1,5 @@
+import { reservations } from './vacation_house.js';
+
 // Update logo text dynamically on window resize and page load
 window.addEventListener('DOMContentLoaded', function () {
     const logoText = document.getElementById('logo_text');
@@ -28,6 +30,43 @@ function toggleLoginPopUp() {
     login_popup.classList.toggle('active');
 }
 
+// display catalogue
+const catalogContainer = document.getElementById("catalog-container");
+
+reservations.forEach(reservation => {
+    const panel = document.createElement("div");
+    panel.className = "panel";
+
+    const link = document.createElement("a");
+    link.href = "../php/reserve.php";
+
+    const image = document.createElement("img");
+    image.src = reservation.photo;
+    image.alt = "Vacation Photo";
+    link.appendChild(image);
+
+    const title = document.createElement("h3");
+    title.textContent = reservation.name;
+    link.appendChild(title);
+
+    const date = document.createElement("p");
+    date.textContent = reservation.dateRange;
+    link.appendChild(date);
+
+    const rating = document.createElement("p");
+    rating.className = "rating";
+    rating.textContent = "★" + reservation.rating.toFixed(1);
+
+    link.appendChild(rating);
+
+    const price = document.createElement("p");
+    price.textContent = reservation.price;
+    link.appendChild(price);
+
+    panel.appendChild(link);
+    catalogContainer.appendChild(panel);
+});
+
 // show/hide footer with animation
 var lastScrollTop = 0;
 var footer = document.querySelector('footer');
@@ -55,3 +94,7 @@ function handleScroll() {
 window.addEventListener('scroll', function () {
     handleScroll();
 });
+
+// update year dynamically
+var year = document.getElementById('year');
+year.innerHTML = new Date().getFullYear();
