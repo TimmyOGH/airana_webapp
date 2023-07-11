54bcadf2d4734f0bf01c95214f7ca7d586b565d9
TimmyOGH <75523747+TimmyOGH@users.noreply.github.com>
2023-07-11 17:52:16 +0630

    Update airanaDB.sql

--- airana/airanaDB.sql
+++ airana/airanaDB.sql
@@ -2,6 +2,50 @@ CREATE DATABASE airanaDB;
 
 USE airanaDB;
 
-CREATE TABLE Admin (aid INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(50) NOT NULL UNIQUE, password VARCHAR(50) NOT NULL);
+CREATE TABLE Admin (
+    aid INT AUTO_INCREMENT PRIMARY KEY, 
+    username VARCHAR(50) NOT NULL UNIQUE, 
+    password VARCHAR(50) NOT NULL);
 
-CREATE TABLE User (uid INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(50) NOT NULL UNIQUE, password VARCHAR(50) NOT NULL, email VARCHAR(50) NOT NULL UNIQUE);
+CREATE TABLE User (
+    uid INT AUTO_INCREMENT PRIMARY KEY, 
+    username VARCHAR(50) NOT NULL UNIQUE, 
+    password VARCHAR(50) NOT NULL, 
+    email VARCHAR(50) NOT NULL UNIQUE);
+
+CREATE TABLE VacationHome (
+    vhid INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    price INT NOT NULL,
+    startDate DATE NOT NULL,
+    endDate DATE NOT NULL
+);
+
+CREATE TABLE Review (
+    revid INT PRIMARY KEY AUTO_INCREMENT,
+    uid INT,
+    review VARCHAR(255),
+    rating DECIMAL(2, 1),
+    vhid INT NOT NULL,
+    FOREIGN KEY (uid) REFERENCES User(uid),
+    FOREIGN KEY (vhid) REFERENCES VacationHome(vhid)
+);
+
+CREATE TABLE Photo (
+    pid INT PRIMARY KEY AUTO_INCREMENT,
+    photo VARCHAR(50) NOT NULL,
+    vhid INT NOT NULL,
+    FOREIGN KEY (vhid) REFERENCES VacationHome(vhid)
+);
+
+CREATE TABLE Photo2 (
+    vhid INT NOT NULL,
+    photo VARCHAR(50) NOT NULL,
+    FOREIGN KEY (vhid) REFERENCES VacationHome(vhid)
+);
+
+CREATE TABLE Photo3 (
+    vhid INT NOT NULL,
+    photo VARCHAR(50) NOT NULL,
+    FOREIGN KEY (vhid) REFERENCES VacationHome(vhid)
+);
